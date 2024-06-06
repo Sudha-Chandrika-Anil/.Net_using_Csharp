5b3b3f41b86b4cb2759c0a49520daa52b4c818b6
Sudha-Chandrika-Anil <141058459+Sudha-Chandrika-Anil@users.noreply.github.com>
2024-06-06 23:57:58 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace HelloWorld
+{
+	public class Class1
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World");
+            Console.WriteLine("I'm learning C#");
+            Console.WriteLine("It is awesome");
+            Console.WriteLine(2+6);
+        }
+    }
+}

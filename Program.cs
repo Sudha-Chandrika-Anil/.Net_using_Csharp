7db0fd25baf6773e4b623e1598546bc040d060d4
Sudha-Chandrika-Anil <141058459+Sudha-Chandrika-Anil@users.noreply.github.com>
2024-06-12 23:20:00 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MobileApp
+{
+    class Program
+    {
+        public void Main(string[] args)
+        {
+            Android mob = new Android("Apple","IPhone","Android");
+            int amt;
+            string ph;
+            Console.WriteLine("Enter the Phone Number");
+            ph = Console.ReadLine();
+            mob.makeCall(ph);
+            Console.WriteLine("Enter the level to charge battery");
+            amt = int.Parse(Console.ReadLine());
+            mob.chargeBattery(amt);
+            Console.WriteLine("Enter the level to use battery");
+            amt = int.Parse(Console.ReadLine());
+            mob.useBattery(amt);
+            mob.printDetails();
+        }
+    }
+}

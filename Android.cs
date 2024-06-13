82bf9d5d3568cf9d04c1aa45b19ee9dd92b8eb1d
Sudha-Chandrika-Anil <141058459+Sudha-Chandrika-Anil@users.noreply.github.com>
2024-06-13 23:43:29 +0530

    Add files via upload

---
+++ Android.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MobileApp
+{
+    public class Android:Mobile,icamera,igps
+    {
+        public string os;
+        public Android(string brand,string model,string os):base(brand,model)
+        {
+            this.os = os;
+        }
+        public void printDetails()
+        {
+            Console.WriteLine(base.brand);
+            Console.WriteLine(base.model);
+            Console.WriteLine(base.battery_level);
+            Console.WriteLine(os);
+            Console.ReadLine();
+        }
+
+        void takePhoto()
+        {
+
+        }
+        void getLocation()
+        {
+
+        }
+    }
+}

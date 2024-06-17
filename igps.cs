f0f52be029235b2d872eebc811a0d75861c93252
Sudha-Chandrika-Anil <141058459+Sudha-Chandrika-Anil@users.noreply.github.com>
2024-06-17 23:54:41 +0530

    Add files via upload

---
+++ igps.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MobileApp
+{
+    public interface igps
+    {
+        void getLocation();
+    }
+}

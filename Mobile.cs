63983537bb1baa8c6c73d30b1a65f26314d3855b
Sudha-Chandrika-Anil <141058459+Sudha-Chandrika-Anil@users.noreply.github.com>
2024-06-23 23:47:49 +0530

    Add files via upload

--- Mobile.cs
+++ Mobile.cs
@@ -1,54 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace MobileApp
 {
-    public class Mobile
+    public abstract class Mobile
     {
-        public string brand;
-        public string model;
-        public int battery_level;
+        protected String brand;
+        protected String model;
+        protected int batteryLevel;
 
-        public Mobile(string brand,string model)
+        public Mobile(String brand, String model)
         {
             this.brand = brand;
             this.model = model;
-            this.battery_level = 0;
+            this.batteryLevel = 0;
         }
-        public void makeCall(String mobNo)
+        public abstract void StartDevice();
+        public abstract void UseDevice();
+        public virtual void ShowDetails()
         {
-            Console.WriteLine(mobNo);
-            Console.ReadLine();
-        }
-        public void chargeBattery(int amt)
-        {
-            if ((battery_level + amt) <= 100)
-            
-                battery_level += amt;
-            
-            else
-                battery_level = 100;
-        }
-        public void useBattery(int amt)
-        {
-            if (battery_level > 0)
-            {
-                battery_level -= amt;
-            }
-            else
-            { 
-                Console.WriteLine("Charge Phone");
-                Console.ReadLine();
-            }   
-        }
-        public void printDetails()
-        {
-            Console.WriteLine("Brand: "+ brand);
-            Console.WriteLine("Model: "+ model);
-            Console.WriteLine("Battery Level: "+ battery_level);
-            Console.ReadLine();
+            Console.WriteLine("Brand: " + brand + "\nModel: " + model + "\nBattery Level: " + batteryLevel);
         }
     }
 }

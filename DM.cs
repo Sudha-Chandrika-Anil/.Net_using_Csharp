9db4cc60660b5e290b5b6e8d49c0862123a857b1
Sudha-Chandrika-Anil <141058459+Sudha-Chandrika-Anil@users.noreply.github.com>
2024-06-27 23:45:53 +0530

    Add files via upload

---
+++ DM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+delegate void MDelegate();
+namespace MutiDelegate
+{
+    internal class DM
+    {
+        static public void Display()
+        {
+            Console.WriteLine("New Delhi");
+        }
+        static public void Print()
+        {
+            Console.WriteLine("London");
+        }
+    }
+}
